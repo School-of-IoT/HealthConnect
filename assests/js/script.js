4ae70f0039260349e552e57424199901de72e463
Harnish Savsani <68416821+HarnishSavsani@users.noreply.github.com>
2022-03-06 10:18:37 +0530

    added doctor panel and some changes

--- assests/js/script.js
+++ assests/js/script.js
@@ -12,3 +12,35 @@ function view() {
 	document.getElementById('a').setAttribute('style', 'display:block'); 
 } 
 
+ 
+ var scriptUploaded = false;
+ var myLandbot;function openLandbot() {
+
+     if (scriptUploaded) {
+
+         myLandbot.open()
+
+     }
+
+     else {
+
+         var script = document.createElement("script");
+         script.type = "text/javascript";
+         script.setAttribute("SameSite", "None; Secure");
+         script.onload = triggerLandbot()
+         script.src = "https://static.landbot.io/landbot-3/landbot-3.0.0.js"; document.head.appendChild(script);
+
+     }
+ }function triggerLandbot() {
+
+     setTimeout(function () {
+         var script = document.createElement("script");script.innerHTML = `myLandbot = new Landbot.Popup({
+configUrl: 'https://chats.landbot.io/v3/XXXXXXXXXXXXXXXXX/index.json',});`;
+         document.body.appendChild(script);
+         scriptUploaded = true;
+         myLandbot.onLoad(function () {
+             myLandbot.open()
+         })
+     }, 3000)
+
+ }
