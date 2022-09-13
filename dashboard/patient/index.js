cf2d19fe1336416fd11029cd1666264373cccdb3
Akshayan Sinha <akshayan.sinha@gmail.com>
2022-09-13 12:16:08 +0530

    added log out feature

---
+++ dashboard/patient/index.js
@@ -0,0 +1,11 @@
+(function ($) {
+    "use strict";
+
+    
+    $(".log_out").click(function(){
+      sessionStorage.removeItem('uid)';
+      location.href = "../login/"
+    });
+
+
+})(jQuery);
