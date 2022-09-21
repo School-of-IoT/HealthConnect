4a65ed81d59d8d22bdd7e6fc5aea83b2fe6c5a69
Akshayan Sinha <akshayan.sinha@gmail.com>
2022-09-21 10:48:54 +0530

    all graph-data random view

--- dashboard/patient/js/diagnosis.js
+++ dashboard/patient/js/diagnosis.js
@@ -1,19 +1,18 @@
 var arrayLength = 60
-var newArray = []
+var newArray1 = []
+var newArray2 = []
+var newArray3 = []
+var newArray4 = []
 
 for(var i = 0; i < arrayLength; i++) {
-  var y1 = Math.round(Math.random()*10) + 1
-  var y2 = Math.round(Math.random()*10) + 1
-  var y3 = Math.round(Math.random()*10) + 1
-  var y4 = Math.round(Math.random()*10) + 1
-  newArray[i] = y1
-  newArray[i] = y2
-  newArray[i] = y3
-  newArray[i] = y4
+  newArray1[i] = Math.round(Math.random()*10) + 1
+  newArray2[i] = Math.round(Math.random()*10) + 1
+  newArray3[i] = Math.round(Math.random()*10) + 1
+  newArray4[i] = Math.round(Math.random()*10) + 1
 }
 
 Plotly.plot('sys-heart-graph', [{
-  y1: newArray,
+  y: newArray1,
   mode: 'lines',
   line: {
     color: '#80CAF6',
@@ -22,7 +21,7 @@ Plotly.plot('sys-heart-graph', [{
 }]);
 
 Plotly.plot('dia-heart-graph', [{
-  y2: newArray,
+  y: newArray2,
   mode: 'lines',
   line: {
     color: '#f680c1',
@@ -31,7 +30,7 @@ Plotly.plot('dia-heart-graph', [{
 }]);
 
 Plotly.plot('spo2-graph', [{
-  y3: newArray,
+  y: newArray3,
   mode: 'lines',
   line: {
     color: '#bf80f6',
@@ -40,7 +39,7 @@ Plotly.plot('spo2-graph', [{
 }]);
 
 Plotly.plot('resp-graph', [{
-  y4: newArray,
+  y: newArray4,
   mode: 'lines',
   line: {
     color: '#a4e864',
@@ -52,19 +51,38 @@ var cnt = 0;
 
 var interval = setInterval(function() {
   
-  var y = Math.round(Math.random()*10) + 1
-  newArray = newArray1.concat(y)
-  newArray.splice(0, 1)
+  var y1 = Math.round(Math.random()*10) + 1
+  var y2 = Math.round(Math.random()*10) + 1
+  var y3 = Math.round(Math.random()*10) + 1
+  var y4 = Math.round(Math.random()*10) + 1
   
-  var data_update = {
-    y: [newArray]
+  newArray1 = newArray1.concat(y1)
+  newArray1.splice(0, 1)
+  newArray2 = newArray2.concat(y2)
+  newArray2.splice(0, 1)
+  newArray3 = newArray3.concat(y3)
+  newArray3.splice(0, 1)
+  newArray4 = newArray4.concat(y4)
+  newArray4.splice(0, 1)
+
+  var data_update1 = {
+    y: [newArray1]
+  };
+  var data_update2 = {
+    y: [newArray2]
+  };
+  var data_update3 = {
+    y: [newArray3]
+  };
+  var data_update4 = {
+    y: [newArray4]
   };
 
   
-  Plotly.update('sys-heart-graph', data_update)
-  Plotly.update('dia-heart-graph', data_update)
-  Plotly.update('spo2-graph', data_update)
-  Plotly.update('resp-graph', data_update)
+  Plotly.update('sys-heart-graph', data_update1)
+  Plotly.update('dia-heart-graph', data_update2)
+  Plotly.update('spo2-graph', data_update3)
+  Plotly.update('resp-graph', data_update4)
   
   if(cnt === 20) clearInterval(interval);
 }, 500); 
