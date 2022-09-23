d5a1c15604d5b68a5d5f8436dc1beb442db385d9
Akshayan Sinha <akshayan.sinha@gmail.com>
2022-09-23 11:10:40 +0530

    sb-icon changes

--- dashboard/patient/js/sidebar.js
+++ dashboard/patient/js/sidebar.js
@@ -1,3 +1,19 @@
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function () {
+  sidebar.classList.toggle("active");
+  if (sidebar.classList.contains("active")) {
+    sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+  } else
+    sidebarBtn.classList.replace("bx-menu-alt-left", "bx-menu");
+}
+
+function view() { 
+	document.getElementById('a').setAttribute('style', 'display:block'); 
+} 
+
+
+
 (function ($) {
     "use strict";
 
