8965970d01294222a6eb9dee6079cafdbf4815f3
Akshayan Sinha <akshayan.sinha@gmail.com>
2022-09-26 18:01:33 +0530

    added search function

---
+++ dashboard/patient/js/device.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementsByClassName("s-inpt");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("dev-table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
