324d29a6b79cbd146cd7ffbb3c598ebfafe81d86
Akshayan Sinha <akshayan.sinha@gmail.com>
2022-11-14 22:35:42 +0530

    device empty1

--- dashboard/patient/js/index.js
+++ dashboard/patient/js/index.js
@@ -1,47 +1,24 @@
-var patID="";
 
 (function ($) {
     "use strict";
-
-    
-    var user = sessionStorage.getItem('user');
-    var token = sessionStorage.getItem('token');
-    let formData = {
-            user: user,
-            token: token,
-          };
-    
-    $.ajax({
-        type: "GET",
-        url: "https://healthconnect-server.herokuapp.com/data",
-        data : formData,
-        crossDomain: true,
-        dataType: "json",
-        encode: true,
-      }).done(function (data) {
-        patID=data.patient[0]._id;
-        $('.username').text(data.patient[0].Name);
-        $('.chills').text(data.patient[0].Chills);
-        $('.dbp').text(data.patient[0].DBP);
-        $('.sbp').text(data.patient[0].SBP);
-        $('.heartrate').text(data.patient[0].HeartRate);
-        $('.respiration').text(data.patient[0].RR);
-        $('.spo2').text(data.patient[0].SpO2);
-        $('.bloodg').text(data.patient[0].BGroup);
-        $('.temp').text(data.patient[0].Temp);
-        $('.ambulation').text(data.patient[0].Ambulation);
-        $('.fever').text(data.patient[0].HistoryFever);
-        $('.bmi').text(data.patient[0].BMI);
-        $('.fio2').text(data.patient[0].FiO2);
-      }).fail(function (data) {
-        //console.log("failed");
-        window.location.href="../../login/"
-      });
-    
     
     $(".log_out").click(function(){
       sessionStorage.removeItem('uid');
       location.href = "../../login/"
+    });
+
+    $("#dev-table-body").each(function()
+    {
+        if($(this).children("tr").length == 0)
+        {
+          $('.no-device').show();
+          $('.dev-list .box').hide();
+          
+        }
+        else{
+          $('.no-device').hide();
+          $('.dev-list .box').show();
+        }
     });
 
 
@@ -78,43 +55,43 @@ var patID="";
       
       $('#meeting-time').attr('min', min_meet_time);
 
-      function updatePatient(){
+      // function updatePatient(){
 
-        let url = "https://healthconnect-server.herokuapp.com/patient/update/" + patID;
+      //   let url = "https://healthconnect-server.herokuapp.com/patient/update/" + patID;
 
-        let formData = 
-        {
-          "patientData":{
-              "SBP":	$('.sbp.number').text(),
-              "DBP":  $('.dbp.number').text(),
-              "HeartRate": $('.heartrate.number').text(),
-              "RR": $('.respiration.number').text(),
-              "SpO2": $('.spo2.number').text(),
-              "Temp": $('.temp.number').text(),
-              "FiO2": $('.fio2.number').text()
-          }
-        };
+      //   let formData = 
+      //   {
+      //     "patientData":{
+      //         "SBP":	$('.sbp.number').text(),
+      //         "DBP":  $('.dbp.number').text(),
+      //         "HeartRate": $('.heartrate.number').text(),
+      //         "RR": $('.respiration.number').text(),
+      //         "SpO2": $('.spo2.number').text(),
+      //         "Temp": $('.temp.number').text(),
+      //         "FiO2": $('.fio2.number').text()
+      //     }
+      //   };
 
         
 
-        $.ajax({
-          type: "PUT",
-          url: url,
-          data : JSON.stringify(formData),
-          crossDomain: true,
-          dataType: "json",
-          encode: true,
-          headers: {
-            "Content-Type": "application/json"
-          },
-          processData: false,
-        }).done(function (data) {
-          console.log("Updated");
+      //   $.ajax({
+      //     type: "PUT",
+      //     url: url,
+      //     data : JSON.stringify(formData),
+      //     crossDomain: true,
+      //     dataType: "json",
+      //     encode: true,
+      //     headers: {
+      //       "Content-Type": "application/json"
+      //     },
+      //     processData: false,
+      //   }).done(function (data) {
+      //     console.log("Updated");
           
-        }).fail(function (data) {
-          console.log("update failed");
+      //   }).fail(function (data) {
+      //     console.log("update failed");
           
-        });
+      //   });
 
       }
 
