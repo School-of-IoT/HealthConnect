1f6219cd84d5e23f0c5ffb77b1a0563304c896be
Akshayan Sinha <akshayan.sinha@gmail.com>
2023-01-16 10:40:11 +0530

    err chk 1

--- dashboard/patient/js/login.js
+++ dashboard/patient/js/login.js
@@ -39,6 +39,7 @@
         preLoaderHandler();
         
         let len = data.patient[0].devices.length;
+        console.log(len);
         for(let i=0; i<len; i++){
           insert_node(data.patient[0].devices[i].lastUp, data.patient[0].devices[i].node.slice(5), data.patient[0].devices[i].attribute, data.patient[0].devices[i].type);
         }
